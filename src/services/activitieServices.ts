fc1143e3abf1ba003e35779bb4808a66ddc2954b
Ali El-Zayat <alielzayat07@gmail.com>
2025-06-04 05:01:22 +0300

    feat: ✨ Add activity management methods to ActivitieServices
    - Implemented addActivity method for creating new activities.
    - Added getActivityById method to retrieve activities by ID.
    - Introduced updateActivity method for modifying existing activities.
    - Created deleteActivity method for removing activities.

--- src/services/activitieServices.ts
+++ src/services/activitieServices.ts
@@ -7,5 +7,17 @@ class ActivitieServices {
   async getActivities() {
     return await http.get(apiUrlConstants.activities);
   }
+  async addActivity() {
+    return await http.post(apiUrlConstants.activities);
+  }
+  async getActivityById(id: string) {
+    return await http.get(`${apiUrlConstants.activities}/${id}`);
+  }
+  async updateActivity(id: string) {
+    return await http.put(`${apiUrlConstants.activities}/${id}`);
+  }
+  async deleteActivity(id: string) {
+    return await http.delete(`${apiUrlConstants.activities}/${id}`);
+  }
 }
 export default new ActivitieServices();
